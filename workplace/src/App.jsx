2c4c9faaa14e6962af555784887dac3d283ef988
itaitoshiki <toshiki.i0918@gmail.com>
2021-05-16 00:07:05 +0900

    liff追加

--- workplace/src/App.jsx
+++ workplace/src/App.jsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@material-ui/styles";
 import EventEntry from "./EventEntry";
 import Event from "./Event";
 import InputDates from "./InputDates";
+import liff from "@line/liff"; // 追加
 
 import "./assets/styles/App.css";
 
@@ -22,6 +23,52 @@ const theme = createMuiTheme({
 });
 
 const App = () => {
+  /* 追加: メッセージ送信 */
+  const sendMessage = () => {
+    liff
+      .init({ liffId: "1655990844-zDNWJmmx" }) // LIFF IDをセットする
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login({}); // ログインしていなければ最初にログインする
+        } else if (liff.isInClient()) {
+          // LIFFので動いているのであれば
+          liff
+            .sendMessages([
+              {
+                // メッセージを送信する
+                type: "text",
+                text: "You've successfully sent a message! Hooray!",
+              },
+            ])
+            .then(function () {
+              window.alert("Message sent");
+            })
+            .catch(function (error) {
+              window.alert("Error sending message: " + error);
+            });
+        }
+      });
+  };
+
+  /* 追加: UserProfileをAlertで表示 */
+  const getUserInfo = () => {
+    liff.init({ liffId: "1655990844-zDNWJmmx" }).then(() => {
+      if (!liff.isLoggedIn()) {
+        liff.login({}); // ログインしていなければ最初にログインする
+      } else if (liff.isInClient()) {
+        liff
+          .getProfile() // ユーザ情報を取得する
+          .then((profile) => {
+            const userId = profile.userId;
+            const displayName = profile.displayName;
+            alert(`Name: ${displayName}, userId: ${userId}`);
+          })
+          .catch(function (error) {
+            window.alert("Error sending message: " + error);
+          });
+      }
+    });
+  };
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
@@ -45,6 +92,12 @@ const App = () => {
             alignItems="center"
             justify="flex-start"
           >
+            <button className="button" onClick={sendMessage}>
+              send message
+            </button>{" "}
+            <button className="button" onClick={getUserInfo}>
+              show user info
+            </button>{" "}
             <Route exact path="/" component={EventEntry} />
             <Route exact path="/event/:id" component={Event} />
             <Route exact path="/event/:id/input" component={InputDates} />
